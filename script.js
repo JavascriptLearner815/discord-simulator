bacbc2a301285cfd7c24d86f996e486c71cd81f7
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-05-15 17:31:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 {
+console.time("browser checks")
 let { userAgent } = window.navigator
 userAgent = userAgent.toLowerCase()
 const msieCheck1 = userAgent.includes("msie")
@@ -7,5 +8,7 @@ const msieCheck = msieCheck1 || msieCheck2
 if (msieCheck) {
   alert("Our CSS styling probably doesn't work on MSIE. Please switch your browser to Edge or other.")
 }
+console.timeEnd("browser checks")
+// crap here
 console.timeEnd("load")
 }
