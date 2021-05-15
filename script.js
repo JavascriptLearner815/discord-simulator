1fec3aeb9fd46d1dae94be148974fa8c623cf7de
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-05-15 17:26:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 {
-// crap here
+let { userAgent } = window.navigator
+userAgent = userAgent.toLowerCase()
+const msieCheck1 = userAgent.includes("msie")
+const msieCheck2 = userAgent.includes("trident/7.0") && userAgent.includes("rv:11.0") && userAgent.includes("like Gecko")
+const msieCheck = msieCheck1 || msieCheck2
+if (msieCheck) {
+  alert("Our CSS styling probably doesn't work on MSIE. Please switch your browser to Edge or other.")
+}
 console.timeEnd("load")
 }
